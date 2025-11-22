1fa3ecb7222930410236df66dccf01a82664ab96
George Perdrizet <george@perdrizet.org>
2025-11-22 21:57:00 +0000

    Bump version to 1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'Hill Climber'
 copyright = '2025, Hill Climber Contributors'
 author = 'Hill Climber Contributors'
-release = '0.1.14'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
